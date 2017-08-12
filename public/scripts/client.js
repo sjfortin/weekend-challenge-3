cd976c1bf323c70cdc9e9afcab6d489cb8366875
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-12 08:44:00 -0500

    GET request to the database working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,11 +2,26 @@ console.log('client.js sourced');
 
 $(document).ready(function () {
     console.log('jquery sourced');
+    getTasks()
 
-    $('#createTask').on('click', displayTasks)
+    $('#createTask').on('click', getTasks)
 
 });
 
-function displayTasks() {
-    $('#tasks').append('<div class="task-item">' + $('#addTaskInput').val() + '</div>');
+function getTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+        success: function (response) {
+            console.log(response);
+            displayTasks(response)
+        }
+    })
+}
+
+function displayTasks(tasks) {
+    console.log('Display tasks', tasks);
+    tasks.forEach(function(taskItem){
+        $('#tasks').append('<div class="task-item">' + taskItem.task + '</div>');
+    });
 }
