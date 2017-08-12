5c40bb75dc3284d0f1cac8c4fee60ee68a837d27
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-12 08:59:50 -0500

    Created task should be stored in database. POST request. After sending task to db, view should be updated with all tasks

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,24 +4,40 @@ $(document).ready(function () {
     console.log('jquery sourced');
     getTasks()
 
-    $('#createTask').on('click', getTasks)
+    $('#createTask').on('click', saveTasks)
 
 });
+
+function saveTasks() {
+
+    var newTask = {
+        task: $('#addTaskInput').val(),
+        completed: false
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: newTask,
+        success: function (response) {
+            getTasks();
+        }
+    })
+}
 
 function getTasks() {
     $.ajax({
         method: 'GET',
         url: '/tasks',
         success: function (response) {
-            console.log(response);
             displayTasks(response)
         }
     })
 }
 
 function displayTasks(tasks) {
+    $('#tasks').empty();
     console.log('Display tasks', tasks);
-    tasks.forEach(function(taskItem){
+    tasks.forEach(function (taskItem) {
         $('#tasks').append('<div class="task-item">' + taskItem.task + '</div>');
     });
 }
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,15 +2,31 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/pool');
 
-router.get('/', function (req, res) {
-    // Add a SELECT query
+router.post('/', function (req, res) {
     pool.connect(function (errorConnectingToDatabase, client, done) {
         if (errorConnectingToDatabase) {
-            // when connecting to database failed
             console.log('Error connecting to database', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
-            // when connecting to database worked!
+            client.query('INSERT INTO tasks (task, completed) VALUES ($1, $2);', [req.body.task, req.body.completed], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
+router.get('/', function (req, res) {
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
             client.query('SELECT * FROM tasks;', function (errorMakingQuery, result) {
                 done();
                 if (errorMakingQuery) {
